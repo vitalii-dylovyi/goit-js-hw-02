85d475f8ef8cae6021285ed500b7b447cfefa68a
Vitalii Dylovyi <vitalii.dylovyi@gmail.com>
2025-03-26 00:55:38 +0200

    fix task-1

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,5 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  const totalPrice = quantity + pricePerDroid;
+  const totalPrice = quantity * pricePerDroid;
 
   if (totalPrice > customerCredits) {
     return "Insufficient funds!";
